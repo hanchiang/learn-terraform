6e2008b3dcd53995847fa267cdf1f2b7735dbea2
han chiang <yaphc@hotmail.com>
2022-06-12 21:54:13 +0800

    Move route table below subnet

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -45,32 +45,6 @@ resource "aws_internet_gateway" "gateway" {
   }
 }
 
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name   = "public"
-    System = var.tag_name
-  }
-}
-
-resource "aws_route" "public" {
-  route_table_id = aws_route_table.public.id
-
-  destination_cidr_block = var.all_ipv4_cidr
-  gateway_id             = aws_internet_gateway.gateway.id
-}
-
-resource "aws_route_table_association" "public_1" {
-  subnet_id      = aws_subnet.public_1.id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_route_table_association" "public_2" {
-  subnet_id      = aws_subnet.public_2.id
-  route_table_id = aws_route_table.public.id
-}
-
 resource "aws_subnet" "public_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
@@ -115,6 +89,32 @@ resource "aws_subnet" "private_2" {
     Name   = "public 1b"
     System = var.tag_name
   }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name   = "public"
+    System = var.tag_name
+  }
+}
+
+resource "aws_route" "public" {
+  route_table_id = aws_route_table.public.id
+
+  destination_cidr_block = var.all_ipv4_cidr
+  gateway_id             = aws_internet_gateway.gateway.id
+}
+
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public.id
 }
 
 
